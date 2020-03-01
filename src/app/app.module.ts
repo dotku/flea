57b1ad246ee09f3c7d19101916fd2655f6d89262
Weijing Lin <weijing.lin@reeftechnology.com>
2020-02-29 23:29:52 -0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
